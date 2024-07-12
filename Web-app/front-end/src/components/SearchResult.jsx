3e73e9440a49af53630112a2d6c6e61344d33cde
vincenzo10b <vincimon00@outlook.com>
2024-07-12 18:19:24 +0200

    Fix del component SearchBarResult

--- Web-app/front-end/src/components/SearchResult.jsx
+++ Web-app/front-end/src/components/SearchResult.jsx
@@ -2,15 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import '../style/searchResult.css'
 
-<<<<<<< Updated upstream
-export const SearchResult = ({ result }) => {
-=======
-/*export const SearchResult = ({result}) => {
-  return <div className='search-result'>{result.model}</div>;
-};*/
-
-export const SearchResult = ({ result, clearResults }) => {
->>>>>>> Stashed changes
+export const SearchResult = ({ result, clearResults}) => {
   return (
     <Link to={`/phone/${result._id}`} className='search-result' onClick={clearResults}>
       {result.model}
